2d4ddd9f6fae6738d04bfe5fd23190ff6cacaa38
paulmaxgithub <ipaulmax@icloud.com>
2024-03-20 16:29:05 +0300

    Initial Commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by PAULMAX iOS DEV on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
